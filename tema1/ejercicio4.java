5ff3127589ef878eb87b2f007177ce2eff354455
kerrigan17 <celiatr.17@gmail.com>
2023-09-26 10:54:37 +0200

    .

---
+++ tema1/ejercicio4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Dame el primer numero");
+        int n1 = in.nextInt();
+        System.out.println("Dame el segundo numero");
+        int n2 = in.nextInt();
+        System.out.println("Dame el tercer numero");
+        int n3 = in.nextInt();
+
+        if (n1 > n2 && n1 > n3){
+            System.out.println("El numero " + n1 + "es mayor");
+        }else if (n2 > n1 && n2 > n3) {
+
+        }
+
+    }
+}
