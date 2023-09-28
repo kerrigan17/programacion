05fe105a593afd9d4590a8b64e82f309fca7271e
kerrigan17 <celiatr.17@gmail.com>
2023-09-28 09:42:26 +0200

    .

---
+++ tema1/ejercicio13.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ejercicio13 {
+        public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Dame segundos: ");
+        int seg = in.nextInt();
+        System.out.println("Dame minutos: ");
+        int min = in.nextInt();
+        System.out.println("Dame horas: ");
+        int hor = in.nextInt();
+
+        seg ++;
+
+        if (seg == 60){
+                seg = 0;
+                min ++;
+                if( min == 60) {
+                        min = 0;
+                        hor ++;
+                        if ( hor == 24){
+                                hor = 0;
+
+                        }
+                }
+        }
+
+        System.out.println("Hora con un segundo más: " + hor + ":" + min + ":"+ seg );
+
+    }
+}
