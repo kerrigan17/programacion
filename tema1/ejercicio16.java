d99d87c41c93a35ac31c6d23ca770bd5c6094478
Celia <116483564+kerrigan17@users.noreply.github.com>
2023-09-30 21:39:02 +0200

    Update ejercicio16.java

--- tema1/ejercicio16.java
+++ tema1/ejercicio16.java
@@ -3,13 +3,22 @@ import java.util.Scanner;
 public class ejercicio16 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Si quieres de euros a dolares escribe E y si quieres de euros a dolares escribe D");
+        System.out.println("Si quieres de euros a dolares escribe D y si quieres dolares a euros escribe E");
         int de = in.nextInt();
         System.out.println("¿Cuánto dinero?");
         int dinero = in.nextInt();
 
-        if (de == ){
+        double dolares = n1 * 1.06;
+        double euros = n1 * 0.95; 
 
+        if (de == D){
+            System.out.println(de + " euros, son " + dolares);
+        }if else(de == E){
+            System.out.println(de + " dolares, son " + euros);
+
+        }else{
+            System.out.println("Tienes que poner 'D' o 'E'.")
+            
         }
     }
 }
