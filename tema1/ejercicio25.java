fb92cc92f625782aeb7760632a02a43364e59449
kerrigan17 <celiatr.17@gmail.com>
2023-10-04 13:57:28 +0200

    .

--- tema1/ejercicio25.java
+++ tema1/ejercicio25.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class ejercicio25 {
-    
+    public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Dame un numero y te doy su factorial");
+        int numero = in.nextInt();
+        int suma = 1;
+        for (int i = 1; i <= numero; i++){
+            suma = suma * i;
+            System.out.println(suma);
+
+        }
+    }
 }
---
+++ tema1/ejercicio26.java
@@ -0,0 +1,3 @@
+public class ejercicio26 {
+    public static void main (String[] args);
+}
