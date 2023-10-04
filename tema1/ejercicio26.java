be89c64551669b608c6786118c7d5e4bd973c5f1
kerrigan17 <celiatr.17@gmail.com>
2023-10-04 14:05:13 +0200

    .

--- tema1/ejercicio26.java
+++ tema1/ejercicio26.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+
 public class ejercicio26 {
-    public static void main (String[] args);
+    public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+
+    }
 }
