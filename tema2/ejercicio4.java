8a59b590206322689b516b538f1661d8d002d07a
kerrigan17 <celiatr.17@gmail.com>
2023-10-19 11:56:18 +0200

    .

--- tema2/ejercicio4.java
+++ tema2/ejercicio4.java
@@ -3,11 +3,19 @@ package tema2;
 import java.util.Scanner;
 
 public class ejercicio4 {
-    public static String menu (String letter){
+    public static double euro2dollar(double num){
+        return num * 1.05;
+    }
+
+    public  static double dollartoeuro(double num){
+        return num * 0.95;
+    }
+    public static String menu (char de){
         if (letter =="D"){
-            return double dolar;
+            return  dollartoeuro(double dinero);
         }if (letter == "E"){
-            return double euro;
+            double resultado = de*num;
+            return euro2dollar(double dinero);
         }else{
             System.out.println("Tienes que poner 'D' o 'E'.");
 
@@ -20,6 +28,11 @@ public class ejercicio4 {
         char de = in.next().charAt(0);
         System.out.println("¿Cuánto dinero?");
         double dinero = in.nextInt();
+        if (de == 'e'){
+            return euro2dollar(double num);
+            System.out.println("Tu conversion es:" + euro2dollar());
+
+        }
 
 
     }
